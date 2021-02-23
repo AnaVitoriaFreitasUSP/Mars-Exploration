c917d5d86555e5aa7f45e0c943104d470983f057
AnaVitoriaFreitasUSP <anavitoria_gouvea@usp.br>
2021-02-23 10:09:14 -0300

    need error changes

--- code/Code.java
+++ code/Code.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class NaveInfo{
+class ShipInfo{
     int x0;
     int y0;
     char direction;
@@ -29,23 +29,22 @@ class Direction{
             case 'n':
                 this.n = "nwse"; 
 
-                break;
+            break;
             
             case 'e':
 
                 this.e = "enws";
                 
-                break;
+            break;
 
             case 's':
                 this.s = "senw";
 
-                break;
+            break;
 
             case 'w':
                 this.w = "wsen";
-
-                break;
+            break;
         }
         
         if(direction == 'n'){
@@ -59,33 +58,35 @@ class Direction{
         }
     }
 
-    int[] getCordinates(char direction, int x, int y){
-        int[] coordenadas;
-        coordenadas = new int[2]; 
+    int[] getCoordinates(char direction, int x, int y){
+        int[] coordinates;
+        coordinates = new int[2]; 
         if(direction == 'n'){
-            coordenadas[0] = x;
-            coordenadas[1] = y + 1;
+            coordinates[0] = x;
+            coordinates[1] = y + 1;
         }else if(direction == 'e'){
-            coordenadas[0] = x + 1;
-            coordenadas[1] = y;
+            coordinates[0] = x + 1;
+            coordinates[1] = y;
         }else if(direction == 's'){
-            coordenadas[0] = x;
-            coordenadas[1] = y -1;
+            coordinates[0] = x;
+            coordinates[1] = y -1;
         }else{
-            coordenadas[0] = x - 1;
-            coordenadas[1] = y;
+            coordinates[0] = x - 1;
+            coordinates[1] = y;
         }
 
-        return coordenadas;
+        return coordinates;
     }
 }
 
 class Exploration{
     Direction exploracao = new Direction();
-    ErrorTreatment erro = new ErrorTreatment();
+
     String moves;
     int height;
     int width;
+    int x;
+    int y;
 
 
     void setHeight(int height){
@@ -100,9 +101,17 @@ class Exploration{
         this.moves = moves;
     }
 
+    int getFinalX(){
+        return this.x;
+    }
+
+    int getFinalY(){
+        return this.y;
+    }
+
     String rotation(char direction){
-        Direction direcao = new Direction();
-        return direcao.getDirection(direction);
+        Direction shipDirection = new Direction();
+        return shipDirection.getDirection(direction);
     }
 
     String finalPosition(char direction, int x0, int y0, int height, int width){
@@ -116,53 +125,87 @@ class Exploration{
         int difference = 0;
 
         for(int i = 0; i < this.moves.length(); i++){
-            if(moves.charAt(i) == 'L'){
+            if(moves.charAt(i) == 'l'){
                 qtL++;
                 difference = qtL - qtR;
 
                 if(difference >= 0){
                     finalDirection = inicialRotation.charAt(difference % 4);
-                }else{
+                }else if(difference < 0 && difference < 4){
                     finalDirection = inicialRotation.charAt((4 + difference) % 4);
+                }else if(difference < 0 && difference > 4){
+                    finalDirection = inicialRotation.charAt((-1 * difference) % 4);
                 }
-            }else if(moves.charAt(i) == 'R'){
+            }else if(moves.charAt(i) == 'r'){
                 qtR++;
                 difference = qtL - qtR;
 
                 if(difference >= 0){
                     finalDirection = inicialRotation.charAt(difference % 4);
-                }else{
+                }else if(difference < 0 && difference < 4){
                     finalDirection = inicialRotation.charAt((4 + difference) % 4);
+                }else if(difference < 0 && difference > 4){
+                    finalDirection = inicialRotation.charAt((-1 * difference) % 4);
                 }
-            }else if(moves.charAt(i) == 'M'){
-                erro.treatInicialPosition(x, y, height, width);
-                x = exploracao.getCordinates(finalDirection, x, y)[0];
-                y = exploracao.getCordinates(finalDirection, x, y)[1];
+            }else if(moves.charAt(i) == 'm'){
+                this.x = x;
+                this.y = y;
+                x = exploracao.getCoordinates(finalDirection, x, y)[0];
+                y = exploracao.getCoordinates(finalDirection, x, y)[1];
 
             }
         }
 
-        return x + " " + y + " " + finalDirection;
+        return x + " " + y + " " + Character.toUpperCase(finalDirection);
 
     }
 }
 
 class ErrorTreatment{
-    void treatInicialPosition(int x, int y, int height, int width){
-        if(x > width || y > height || x < 0 || y < 0){
-            System.out.print("\n===================================================================================\n");
-            System.out.print("Parece que a nave nem Marte está explorando mais! Acabou saindo do perímetro :(\n");
-            System.out.print("=====================================================================================\n");
-        }
 
+    void treatInicialPosition(int x, int y, int height, int width){
         if(height <= 0 || width <= 0){
             System.out.print("\n==========================================================================================\n");
             System.out.print("Parece que algum buraco negro destruiu as dimensões de Marte! São inválidas ou nulas :(\n");
-            System.out.print("============================================================================================\n");
+            System.out.print("==========================================================================================\n");
 
+        }
+        else if(x > width || y > height || x < 0 || y < 0){
+            System.out.print("\n===================================================================================\n");
+            System.out.print("Parece que a nave nem Marte está explorando mais! Acabou saindo do perímetro :(\n");
+            System.out.print("===================================================================================\n");
         }
 
 
+
+    }
+
+}
+
+class Crash{
+    int x;
+    int y;
+
+    void setX(int x){
+        this.x = x;
+    }
+
+    void setY(int y){
+        this.y = y;
+    }
+
+    void checkCrashes(ArrayList <Crash> crash){
+        int flag = 0;
+        for(int i = 0; i < crash.size(); i++){
+            for(int j = i; j < crash.size(); j++){
+                if(crash.get(i).x == crash.get(j).x && crash.get(i).y == crash.get(j).y && flag == 0){
+                    flag = 1;
+                    System.out.print("\n==========================================================================================\n");
+                    System.out.print("A exploração em Marte não foi bem sucedida :( As naves acabaram se chocando :O\n");
+                    System.out.print("==========================================================================================\n");
+                }
+            }
+        }
     }
 }
 
@@ -171,42 +214,56 @@ public class Code {
 
         Scanner s = new Scanner(System.in);
 
-        ArrayList<NaveInfo> nave = new ArrayList<NaveInfo>();
-        ArrayList<Exploration> explorando = new ArrayList<Exploration>();
+        ArrayList<ShipInfo> allShips = new ArrayList<ShipInfo>();
+        ArrayList<Exploration> allExplorationInfo = new ArrayList<Exploration>();
+
+
+        System.out.print("Digite as dimensões do terreno (separadas por espaço): \n");
+        int height = s.nextInt();
+        int width = s.nextInt();
         
         
         do{
-            NaveInfo navezinha = new NaveInfo();
-            Exploration explorar = new Exploration();
-            System.out.print("Digite as dimensões do terreno: (separadas por espaço)\n");
-            int height = s.nextInt();
-            explorar.setHeight(height);
-            int width = s.nextInt();
-            explorar.setWidth(width);
+            ShipInfo ship = new ShipInfo();
+            Exploration explorating = new Exploration();
+            explorating.setHeight(height);
+            explorating.setWidth(width);
             
             System.out.print("Digite a posição inicial da nave e a direção em que está (N, E, S, W):\n");
             int x = s.nextInt();
-            navezinha.setX0(x);
+            ship.setX0(x);
             int y = s.nextInt();
-            navezinha.setY0(y);
+            ship.setY0(y);
             String direction = s.nextLine();
-            navezinha.setDirection(direction);
+            ship.setDirection(direction.toLowerCase());
 
             System.out.print("Digite os comandos que a nave deverá realizar:\n");
-            String comands = s.nextLine();
-            explorar.setMoves(comands);
+            String commands = s.nextLine();
+            explorating.setMoves(commands.toLowerCase());
 
-            nave.add(navezinha);
-            explorando.add(explorar);
+            allShips.add(ship);
+            allExplorationInfo.add(explorating);
 
         }while (s.hasNextLine());
 
+        ArrayList<Crash> crashes = new ArrayList<Crash>();
+        Crash shipsCrash = new Crash();
 
-        for(int i = 0; i < nave.size(); i++){
+        for(int i = 0; i < allShips.size(); i++){
+            ErrorTreatment error = new ErrorTreatment();
+            Crash shipPosition = new Crash();
             System.out.print("\n");
-            System.out.print(explorando.get(i).finalPosition(nave.get(i).direction, nave.get(i).x0, nave.get(i).y0, explorando.get(i).height, explorando.get(i).width));
+            System.out.print("Nave " + (i + 1) + ": " + allExplorationInfo.get(i).finalPosition(allShips.get(i).direction, allShips.get(i).x0, allShips.get(i).y0, allExplorationInfo.get(i).height, allExplorationInfo.get(i).width));
             System.out.print("\n");
+            error.treatInicialPosition(allExplorationInfo.get(i).getFinalX(), allExplorationInfo.get(i).getFinalY(), allExplorationInfo.get(i).height, allExplorationInfo.get(i).width);
+            shipPosition.setX(allExplorationInfo.get(i).getFinalX());
+            shipPosition.setY(allExplorationInfo.get(i).getFinalY());
+            crashes.add(shipPosition);
         }
+
+        shipsCrash.checkCrashes(crashes);
+
+
 
 
         s.close();
